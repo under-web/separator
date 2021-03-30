72e17837475c1712e0ecd291d05e11aa02a9ac4d
appdev <>
2021-03-29 23:42:20 -0400

    add phone

--- argentavis.py
+++ argentavis.py
@@ -10,36 +10,41 @@ class Separator:
     Класс для изъятия личных данных из обьектов
     """
 
-    def get_email(self, path):
+    def get_email_file(self, path):
         """
         Возвращает из файла emails в output.txt
         :param path: путь к файлу в котором искать email
         :return: количество строк
         """
-        with open(path, 'r', encoding='utf-8') as file:
-            text = file.read()  # указываем размер считывания из файла
-            pattern = re.compile(r'[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+')  # создали шаблон
-            result = pattern.findall(text)  # указываем что ищем и где ищем
-            lister = []  # пустой список
-            s = 0  # счетчик
-            for i in result:
-                lister.append(i)
-                s += 1
-        # print("Количество строк = ", s)
 
-        with open('output.txt', 'a') as file:
-            for index in lister:
-                file.write(index + '\n')
-        return s
+        with open(path, 'r', encoding='utf-8', errors='ignore') as file:
+            s = 0
+            for lin in file:
+                pattern = re.compile(r'[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+')  # создали шаблон
+                result = pattern.findall(lin)  # указываем что ищем и где ищем
+                with open('output_mail.txt', 'a') as file_out:
+                    file_out.write(''.join(result) + '\n')
+                    s += 1
+            return s
 
-    def get_phone(self, path):
-        pass
+    def get_phone_file(self, path, regex=r'\d{11,12}'):
+        with open(path, 'r', encoding='utf-8', errors='ignore') as file:
+            s = 0
+            for lin in file:
+                print(lin)
+                pattern = re.compile(regex)  # создали шаблон
+                result = pattern.findall(lin)  # указываем что ищем и где ищем
+                if not result:
+                    continue
+                else:
+                    with open('output_phone.txt', 'a') as file_out:
+                        file_out.write(''.join(result) + '\n')
+                        s += 1
+            return s
 
 
-if __name__ != '__main__':
-    pass
-else:
+if __name__ == '__main__':
     # Example
     # name_var = Separator()
-    var = Separator
-    var.get_email('ooo.txt')
+    var = Separator()
+    var.get_phone_file('/home/appdev/Рабочий стол/Текущая/Базы/VK_100M.txt')
